ec207830aef86634dc2b25fce3ccae47c7d39196
coolofdead <thomas.giovannoni@hotmail.fr>
2021-12-13 16:53:19 +0100

    ouai

--- Delivery Guy/Assets/Script/Script GameManager/CompteurCardboard.cs
+++ Delivery Guy/Assets/Script/Script GameManager/CompteurCardboard.cs
@@ -7,16 +7,17 @@ public class CompteurCardboard : MonoBehaviour
     [SerializeField] int nbCarton = 0;
     public int NbCarton { get => nbCarton; }
     public Transform pizzaBoxAnchor;
+    public Transform pizzaBoxScooter;
     public ExampleInput exampleInput;
 
     [Header("Control")]
     public float minRotEfficiency = 0;
     public float maxRotEfficiency = 50;
-    public float posRotOffset = 1;
     public float pizzaBoxY;
     public AnimationCurve rotCurve;
     public AnimationCurve posCurve;
     public float evaluateSmoothness = 0.01f;
+    public float speed = 1;
 
     [Header("")]
     public float pizzaDisapearTime = 0.7f;
@@ -25,21 +26,37 @@ public class CompteurCardboard : MonoBehaviour
 
     private void Update()
     {
-        for (int i = 0; i < pizzaBoxAnchor.childCount; i++)
+        pizzaBoxAnchor.GetChild(0).position = pizzaBoxScooter.position;
+
+        for (int i = 1; i < pizzaBoxAnchor.childCount; i++)
+        {
+            Transform previousPizzaBox = pizzaBoxAnchor.GetChild(i-1);
+            Transform curPizzaBox = pizzaBoxAnchor.GetChild(i);
+
+            curPizzaBox.position = Vector3.Lerp(curPizzaBox.position, previousPizzaBox.position, speed * Time.deltaTime);
+            var pos = curPizzaBox.position;
+            pos.y = pizzaBoxAnchor.position.y + i * pizzaBoxY;
+            curPizzaBox.position = pos;
+        }
+    }
+
+    private void Ouaip()
+    {
+        for (int i = 1; i < pizzaBoxAnchor.childCount; i++)
         {
             var pizzaBox = pizzaBoxAnchor.GetChild(i);
 
             var pizzaEfficiencyPercentage = (float)i / (float)pizzaBoxAnchor.childCount;
 
             var pizzaBoxPos = pizzaBoxAnchor.position;
-            pizzaBoxPos += transform.right * -exampleInput.curRot * posRotOffset * posCurve.Evaluate(pizzaEfficiencyPercentage);
+            pizzaBoxPos += -transform.right * exampleInput.curRot * posCurve.Evaluate(pizzaEfficiencyPercentage);
             pizzaBoxPos.y = pizzaBoxAnchor.position.y + i * pizzaBoxY;
 
             pizzaBox.position = pizzaBoxPos;
             pizzaBox.rotation = pizzaBoxAnchor.rotation;
 
             var efficiency = rotCurve.Evaluate(i * evaluateSmoothness);
-            pizzaBox.Rotate(0, 0, exampleInput.curRot * efficiency, Space.Self);
+            //pizzaBox.Rotate(0, 0, -exampleInput.curRot * efficiency, Space.Self);
         }
     }
 
