fd92399e4fef63484a6cc668249ac4c0555c2e6e
NicolasJeannette <44873578+NicolasJeannette@users.noreply.github.com>
2021-12-17 11:19:32 +0100

    Ajout Note

--- Delivery Guy/Assets/Script/Script Note/ScriptNote.cs
+++ Delivery Guy/Assets/Script/Script Note/ScriptNote.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class ScriptNote : MonoBehaviour
 {
     public static int nbCarton;
     public int noteNecessaire;
+    public int prochainObjectif;
+    public bool lastCrown;
+    public TMP_Text texte;
     public GameObject image;
+    public List<GameObject> textePrecedent;
     // Start is called before the first frame update
     void Start()
     {
         if (nbCarton >= noteNecessaire)
         {
             image.SetActive(true);
+            if (lastCrown == false)
+            {
+                texte.text = $"Prochain objectif : {prochainObjectif}";
+                
+            }
+            else { texte.text = "Vous avez rempli tous les objectifs ! Bravo !"; }
+
+            foreach (var item in textePrecedent)
+            {
+                item.SetActive(false);
+            }
         }
         else
         {
